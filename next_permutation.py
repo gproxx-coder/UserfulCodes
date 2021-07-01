99845857e2787b198efbdecda3db1a2803cb888c
Ganesh Patil <68324277+gproxx-coder@users.noreply.github.com>
2021-07-01 23:09:28 +0530

    all tests passed

--- next_permutation.py
+++ next_permutation.py
@@ -1,12 +1,19 @@
 def next_bigger(num):
+	# Check whether number is in descending order OR same digit or single digit
+	# If yes then retuen -1
+	if len(set(str(num))) == 1 or int("".join(sorted(list(str(num)), reverse=True))) == num:
+		return -1
+
 	right_greater_idx,mid_point = 0,0
-	# Find middle point
+
+	# Find middle point to split the number
 	chars = list(str(num))
 	for idx in range(len(chars)-1,-1,-1):
 		if not chars[idx] <= chars[idx-1]:
 			mid_point = idx
 			break
 
+	# keeping left and right parts seperately and also middle value
 	mid_char = [chars[mid_point-1]]
 	left = chars[:mid_point-1]
 	right = chars[mid_point:]
@@ -16,28 +23,9 @@ def next_bigger(num):
 			right_greater_idx = idx
 			break
 
+	# Swapping the middle point with next greater number from right part
 	mid_char[0], right[right_greater_idx] = right[right_greater_idx], mid_char[0]
 
-	op = int("".join(left) + mid_char[0] + "".join(right))
-	if op == num:
-		return -1
-	else:
-		return op
-
-print(next_bigger(1234567980 ))
-
-def findnext(ii):
-    iis=list(map(int,str(ii)))
-    for i in reversed(range(len(iis))):
-        if i == 0: return ii
-        if iis[i] > iis[i-1] :
-            break        
-    left,right=iis[:i],iis[i:]
-    for k in reversed(range(len(right))):
-        if right[k]>left[-1]:
-           right[k],left[-1]=left[-1],right[k]
-           break
-    return int("".join(map(str,(left+sorted(right)))))
-
-
-print(findnext(1234567980 ))
+	return int("".join(left) + mid_char[0] + "".join(right))
+	
+print(next_bigger(99642))
